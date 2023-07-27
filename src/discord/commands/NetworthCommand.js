fd05210ef3f69cabb475f2b89b10ff597581aaf3
AzaelMew <dogydemon@gmail.com>
2023-07-27 22:59:35 +0300

    asdasd

--- src/discord/commands/NetworthCommand.js
+++ src/discord/commands/NetworthCommand.js
@@ -11,7 +11,28 @@ async function getUUIDFromUsername(username) {
     return data.id
   }
 }
-function numberWithCommas(x) {
+function numberWithCommas(number) {
+  if (number < 1) {
+    return 0
+  }
+  decPlaces = 1
+  decPlaces = Math.pow(10,decPlaces);
+  var abbrev = [ "k", "m", "b", "t" ];
+  for (var i=abbrev.length-1; i>=0; i--) {
+      var size = Math.pow(10,(i+1)*3);
+      if(size <= number) {
+           number = Math.round(number*decPlaces/size)/decPlaces;
+           if((number == 1000) && (i < abbrev.length - 1)) {
+               number = 1;
+               i++;
+           }
+           number += abbrev[i];
+           break;
+      }
+  }
+  return number;
+}
+function mainNum(x) {
   try {
     if (x > 999815672) {
       x = x.toString().split(".")[0]
@@ -64,7 +85,7 @@ async function getNetworthFromUUID(name) {
     let storageec
 
     storageec = ec + storage
-    ret = "**Total**:\n➣ $" + numberWithCommas(total) + ". **Purse:**\n➣ $" + numberWithCommas(purse) + ". **Bank:**\n➣ $" + numberWithCommas(bank) + ". **Armor:**\n➣ $" + numberWithCommas(armor) + ". **Equipment:**\n➣ $" + numberWithCommas(equ) + ". **Wardrobe:**\n➣ $" + numberWithCommas(wardrobe) + ". **Inv:**\n➣ $" + numberWithCommas(inventory) + ". **Storage:**\n➣ $" + numberWithCommas(storageec) + ". **Pets:**\n➣ $" + numberWithCommas(pets) + ". **Talis:**\n➣ $" + numberWithCommas(acc)
+    ret = "**Total**:\n➣ $" + mainNum(total) + "¨ **Purse:**\n➣ $" + numberWithCommas(purse) + "¨ **Bank:**\n➣ $" + numberWithCommas(bank) + "¨ **Armor:**\n➣ $" + numberWithCommas(armor) + "¨ **Equipment:**\n➣ $" + numberWithCommas(equ) + "¨ **Wardrobe:**\n➣ $" + numberWithCommas(wardrobe) + "¨ **Inventory:**\n➣ $" + numberWithCommas(inventory) + "¨ **Storage:**\n➣ $" + numberWithCommas(storageec) + "¨ **Pets:**\n➣ $" + numberWithCommas(pets) + "¨ **Talis:**\n➣ $" + numberWithCommas(acc)
     return ret
 
   }
@@ -106,7 +127,7 @@ class NetworthCommand extends DiscordCommand {
       this.sendMinecraftMessage(`/gc ${user}'s networth: ${ret.replaceAll("\n", "").replaceAll("*","").replaceAll("➣","")}`)
       message.channel.send({
         embeds: [{
-          description: ret.replaceAll(".", "\n").replaceAll(":",""),
+          description: ret.replaceAll("¨", "\n").replaceAll(":",""),
           color: 0x2A2A2A,
           timestamp: new Date(),
           footer: {
